a39b4f185bf3077c628d9f561655312096da59fc
Hasan <hassenbike@gmail.com>
2022-12-09 01:44:27 +0600

    solved Day 8

--- src/DayTemplate.kt
+++ src/DayTemplate.kt
@@ -1,20 +1,17 @@
 fun main() {
     val testInput = readInput("Day01_test")
-    check(Day.part1(testInput) == 1)
+    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
 
-    println(Day.part1(input))
-    println(Day.part2(input))
+    println(part1(input))
+    println(part2(input))
 }
 
-object Day {
+fun part1(input: List<String>): Int {
+    return input.size
+}
 
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun part2(input: List<String>): Int {
+    return input.size
 }
