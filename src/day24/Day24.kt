de61bd52a12b466b010412292e688037118b7305
Hasan <hassenbike@gmail.com>
2023-01-05 16:37:39 +0600

    day 24 finished 1 part

--- src/day24/Day24.kt
+++ src/day24/Day24.kt
@@ -9,26 +9,15 @@ import lib.Vector2D
 import lib.directionsClockwise
 import readInput
 
-
-data class Node(val pos: Point2D, val childs: MutableList<Node> = mutableListOf())
-
-
 fun main() {
     val day = 24
     val testInput = readInput("day$day/testInput")
-//    check(part1(testInput) == 18)
+    check(part1(testInput) == 18)
 
     val input = readInput("day$day/input")
     println(part1(input))
 //    println(part2(input))
 }
-
-val dirToSymbol = mapOf(
-    RightDirection to '>',
-    LeftDirection to '<',
-    IncreaseYDirection to 'v',
-    DecreaseYDirection to '^'
-)
 
 fun part1(input: List<String>): Int {
 
@@ -38,89 +27,66 @@ fun part1(input: List<String>): Int {
     val start = Point2D(1, 0)
     val destination = Point2D(w - 2, h - 1)
 
-    var winds = mutableMapOf<Point2D, MutableSet<Vector2D>>()
+    val winds = mutableListOf<Pair<Point2D, Vector2D>>()
 
     (0 until h).forEach { y ->
         (0 until w).forEach { x ->
             val c = input[y][x]
             val p = Point2D(x, y)
-
             when (c) {
 
-                '>' -> {
-                    winds[p] = mutableSetOf(RightDirection)
-                }
+                '>' -> winds.add(p to RightDirection)
 
-                '<' -> {
-                    winds[p] = mutableSetOf(LeftDirection)
-                }
+                '<' -> winds.add(p to LeftDirection)
 
-                'v' -> {
-                    winds[p] = mutableSetOf(IncreaseYDirection)
-                }
+                'v' -> winds.add(p to IncreaseYDirection)
 
-                '^' -> {
-                    winds[p] = mutableSetOf(DecreaseYDirection)
-                }
+                '^' -> winds.add(p to DecreaseYDirection)
+                else -> {}
+
             }
         }
     }
 
     fun updateWindsPosition() {
-        val newWinds = mutableMapOf<Point2D, MutableSet<Vector2D>>()
+        for (i in winds.indices) {
+            val wind = winds[i]
+            val newPosition = wind.first + wind.second
+            var x = newPosition.x
+            var y = newPosition.y
 
-        for (wind in winds) {
-
-            for (dir: Vector2D in wind.value) {
-                val nextPosition = wind.key + dir
-                var x = nextPosition.x
-                var y = nextPosition.y
-
-                if (x < 1) {
-                    x = (w - 2)
-                }
-
-                if (x > w - 2) {
-                    x = 1
-                }
-
-                if (y < 1) {
-                    y = (h - 2)
-                }
-
-                if (y > h - 2) {
-                    y = 1
-                }
-                val newPosition = Point2D(x, y)
-
-                newWinds.putIfAbsent(newPosition, mutableSetOf())
-                newWinds[newPosition]?.add(dir)
+            if (x < 1) {
+                x = (w - 2)
             }
 
+            if (x > w - 2) {
+                x = 1
+            }
+
+            if (y < 1) {
+                y = (h - 2)
+            }
+
+            if (y > h - 2) {
+                y = 1
+            }
+            winds[i] = Point2D(x, y) to wind.second
         }
-        winds = newWinds
     }
 
     var minute = 0
-
-    var childs = mutableSetOf<Point2D>()
+    val childs = mutableSetOf<Point2D>()
     childs.add(start)
-
     var notFound = true
 
     while (notFound) {
-
         updateWindsPosition()
+
+        val windsPositions = winds.map { it.first }.toSet()
 
         val newChilds = mutableSetOf<Point2D>()
 
         for (childNode in childs) {
-
-            if (childNode == destination) {
-                notFound = false
-            }
-
-            var foundNear = false
 
             directionsClockwise.forEach { dir ->
                 val p = childNode + dir
@@ -129,38 +95,25 @@ fun part1(input: List<String>): Int {
                     notFound = false
                 }
 
-                if (((p.x in 1 until w - 1) && (p.y in 1 until h - 1)) || p == start || p == destination) {
+                if (((p.x in 1 until w - 1) && (p.y in 1 until h - 1)) || p == start) {
 
-                    val windsContains = winds.containsKey(p)
-
-                    if (!windsContains) {
+                    if (!windsPositions.contains(p)) {
                         newChilds.add(p)
-                        foundNear = true
                     }
                 }
             }
-
-            if (!foundNear) {
-                if (!winds.containsKey(childNode)) {
-                    newChilds.add(childNode)
-                }
+            if (!windsPositions.contains(childNode)) {
+                newChilds.add(childNode)
             }
         }
-
-        childs = newChilds
+        childs.clear()
+        childs.addAll(newChilds)
 
         minute++
-        print("Minute: $minute, childCount: ${childs.size}")
-
-        if (childs.isNotEmpty()) {
-            print(", nearest dist: ${childs.minByOrNull { it.manhattanDistanceTo(destination) }!!.manhattanDistanceTo(destination)}")
-        }
-        println()
+        println("Minute: $minute, childCount: ${childs.size}")
     }
-
     return minute
 }
-
 
 
 fun part2(input: List<String>): Int {
